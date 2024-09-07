663093eb04458bd233773d0996f559e29962f246
minwook <zkvn1103@naver.com>
2024-09-07 14:50:03 +0900

    init projects

---
+++ src/test/java/com/growth/everything/EverythingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.growth.everything;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverythingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
